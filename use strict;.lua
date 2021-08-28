f3e8e40711d5a0e18b7c44f79670791351e2e3c5
JoeBidenForPresident <89684737+JoeBidenForPresident@users.noreply.github.com>
2021-08-28 10:58:45 +0200

    files.js
    ks.ls

---
+++ use strict;.lua
@@ -0,0 +1,21 @@
+
+local putinmood = {
+    'Putin is happy',
+    "Putin is upset",
+     "Putin is sad",
+      "Putin is proud",
+       "Putin is surprised that someone is this retarted, Although, He knows how to program",
+}
+local putinMoney = math.random()
+local function putinmoode(happy, upset, sad, proud, surprised, confused) 
+    local potun = {happy, upset, sad, proud, surprised, confused}local mode = {        potun
+    } if putinMoney > 0 then
+        return potun
+    end
+        
+
+end
+
+putinmoode("Putin is happy", "Putin is upset", "Putin is sad", "Putin is proud", "Putin is surprised that someone is this retarted, Although, He knows how to program", "Putin is confused")
+print(putinmoode("Putin is happy", "Putin is upset", "Putin is sad", "Putin is proud", "Putin is surprised that someone is this retarted, Although, He knows how to program", "Putin is confused"))
+getmetatable(putinmood)
